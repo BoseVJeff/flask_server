afea00a811071834db4bbc2f430cedae5fe3e274
BoseV <46490582+BoseVJeff@users.noreply.github.com>
2023-10-15 22:26:52 +0530

    Updated `db_utils.py` to be thread safe. Added proper image uploads.
    Verified to work after a casual tour of the app.
    
    This should make it safe for use with Flask.

--- db_utils.py
+++ db_utils.py
@@ -8,12 +8,20 @@ from werkzeug.datastructures import FileStorage
 from werkzeug.utils import secure_filename
 import os
 import pathlib
+import typing
 
 # Buffer size for file IO
 BUF_SIZE = 65536
 
 # Error Icon
 ERROR_ICON_PATH = os.path.join("static", "error.png")
+
+# Folder inside `static` for images
+IMAGE_FOLDER = "images"
+
+# Type Aliases for typing covenience
+Connection: typing.TypeAlias = sqlite3.Connection
+Cursor: typing.TypeAlias = sqlite3.Cursor
 
 
 class DbType(Enum):
@@ -29,31 +37,40 @@ class UnknownDbTypeException(Exception):
 
 # NOTE: Remember to check and throw an exception if `dbConnection` is None
 class Db:
-    dbConnection: sqlite3.Connection
-    dbCursor: sqlite3.Cursor
+    # Having all function calls share the same connection leads to thread safety issues.
+    # This happens because Flask spawns a seperate thread for each connection/request.
+    # dbConnection: sqlite3.Connection
+    # dbCursor: sqlite3.Cursor
+    dbLocation: str
+    dbType: DbType
 
-    def _connect(self, dbLocation, dbType: DbType = DbType.SQLITE):
+    def _connect(self):
         """Function to connect this class to a database.
 
         This function must be called first before using any other action.
         """
-        match dbType:
+        match self.dbType:
             case DbType.SQLITE:
-                self.dbConnection = sqlite3.connect(dbLocation)
+                return sqlite3.connect(self.dbLocation)
             case _:
-                raise UnknownDbTypeException(message=f"Unknown DbType {dbType}")
+                raise UnknownDbTypeException(message=f"Unknown DbType {self.dbType}")
 
-    def _setCursor(self):
+    def _getCursor(self, dbConnection):
         "This initialises the cursor for any future operations on the database."
-        self.dbCursor = self.dbConnection.cursor()
+        return dbConnection.cursor()
 
     def __init__(self, dbLocation, dbType: DbType = DbType.SQLITE):
         """Initialise the database.
 
         The database is ready to interact with after this function is done running.
         """
-        self._connect(dbLocation=dbLocation, dbType=dbType)
-        self._setCursor()
+        # self._connect(dbLocation=dbLocation, dbType=dbType)
+        # self._setCursor()
+        self.dbLocation = dbLocation
+        self.dbType = dbType
+        imageFolder = pathlib.Path(os.path.join("static", IMAGE_FOLDER))
+        if not imageFolder.exists():
+            imageFolder.mkdir()
 
     # This code is very project specific and will need to be modified
     # TODO: Implement a DB migration function for when we inevtably end up changing DB schema
@@ -73,16 +90,24 @@ class Db:
                       profile_picture TEXT)"""
         )
         # Taken from https://stackoverflow.com/a/41627098
-        atexit.register(self.cleanup)
+        # No longer needed as we no longer mantain a global connection for all requests.
+        # atexit.register(self.cleanup)
         return
 
-    def cleanup(self):
-        self.dbConnection.commit()
-        self.dbConnection.close()
-        return
+    # def cleanup(self):
+    #     self.dbConnection.commit()
+    #     self.dbConnection.close()
+    #     return
 
-    def executeOneQuery(self, sql: str, parameters: dict[str, str] = {}):
-        """Execute ONE SQL query with the given parameters.
+    def _getDb(self) -> tuple[Connection, Cursor]:
+        conn = self._connect()
+        cur = self._getCursor(conn)
+        return (conn, cur)
+
+    def executeOneQuery(
+        self, sql: str, parameters: dict[str, str | None] = {}
+    ) -> tuple[Connection, Cursor]:
+        """Execute ONE SQL query with the given parameters. Returns a handle to the cursor that was used to execute the query.
 
         Use named-style placeholders in SQL queries eg. `INSERT INTO lang VALUES(:name, :year)`.
         Supply params as `({"name": "C", "year": 1972})`.
@@ -91,12 +116,16 @@ class Db:
 
         For docs on dicts, see [docs here](https://docs.python.org/3/library/stdtypes.html#dict).
         """
-        self.dbCursor.execute(sql, parameters)
-        self.dbConnection.commit()
-        return
+        (dbConnection, dbCursor) = self._getDb()
+        dbCursor.execute(sql, parameters)
+        dbConnection.commit()
 
-    def executeManyQuery(self, sql: str, parameters=List[dict[str, str]]) -> None:
-        """Exectue ONE SQL query for each parameter in Iterable.
+        return (dbConnection, dbCursor)
+
+    def executeManyQuery(
+        self, sql: str, parameters=List[dict[str, str | None]]
+    ) -> tuple[Connection, Cursor]:
+        """Exectue ONE SQL query for each parameter in Iterable.  Returns a handle to the cursor that was used to execute the query.
 
         Use named-style placeholders in SQL queries eg. `INSERT INTO lang VALUES(:name, :year)`.
         Supply params as `[{"name": "C", "year": 1972},{"name": "Fortran", "year": 1957}]`.
@@ -105,60 +134,70 @@ class Db:
 
         For docs on dicts, see [docs here](https://docs.python.org/3/library/stdtypes.html#dict).
         """
-        # Ignored issue on floowing line as it is verified by `mypy`.
-        self.dbCursor.executemany(sql, parameters)  # type: ignore
-        self.dbConnection.commit()
-        return
+        (dbConnection, dbCursor) = self._getDb()
+        dbCursor.executemany(sql, parameters)
+        dbConnection.commit()
+        return (dbConnection, dbCursor)
 
-    def executeScript(self, sql_script: str) -> None:
-        """Execute SQL statements in sql_script."""
-        self.dbCursor.execute(sql_script)
-        self.dbConnection.commit()
-        return
+    def executeScript(self, sql_script: str) -> tuple[Connection, Cursor]:
+        """Execute SQL statements in sql_script. Returns a handle to the cursor that was used to execute the query."""
+        (dbConnection, dbCursor) = self._getDb()
+        dbCursor.execute(sql_script)
+        dbConnection.commit()
+        return (dbConnection, dbCursor)
 
-    def getResults(self, count: int | None = None) -> list[Any]:
-        """Returns `count` number of results.
+    def getResults(self, dbCursor: Cursor, count: int | None = None) -> list[Any]:
+        """Returns `count` number of results from the supplied cursor.
 
         If `count` is not defined, all rows are returned.
         """
+        res: list[Any]
         if count is None:
-            return self.dbCursor.fetchall()
+            res = dbCursor.fetchall()
         elif count == 0:
-            return []
+            res = []
         elif count == 1:
-            result = self.dbCursor.fetchone()
-            if result is None:
-                return []
+            res = dbCursor.fetchone()
+            if res is None:
+                res = []
             else:
-                return [result]
+                res = [res]
         else:
-            return self.dbCursor.fetchmany(size=count)
+            res = dbCursor.fetchmany(size=count)
+        return res
 
-    def getAllUsers(self, pwd: bool = False) -> List[dict[str, str]]:
-        """Does NOT return the passwords by default for security reasons.
+    def getAllUsers(self) -> List[dict[str, str]]:
+        """Does NOT return the passwords for security reasons.
 
         To get the password, set `pwd` to `True`.
         """
-        self.executeOneQuery("SELECT username,email,profile_picture FROM users")
-        # Cursor returns data in the order they are requested in
-        return self.getResults()
 
-    # This function accepts a list of names because it is meant to be used in situations like getting a list of friends, etc.
-    def getUser(self, usernames: List[str]) -> dict[str, str] | None:
+        (dbConn, dbCursor) = self.executeOneQuery(
+            "SELECT username,email,profile_picture FROM users"
+        )
+        # Cursor returns data in the order they are requested in
+        res = self.getResults(dbCursor)
+        dbConn.close()
+        return res
+
+    def getUser(self, username: str) -> dict[str, str] | None:
         """Get details for user having specific username.
 
         Returns `None` if no matching entries are found.
+
+        This searches one user only as `executeManyQuery` can only execute DML queries.
         """
-        self.executeManyQuery(
-            "SELECT username,email,profile_picture FROM users WHERE username=:userName",
-            # Ignoring as this is verified by `mypy`.
-            [{"username": x} for x in usernames],  # type: ignore
+        (dbConn, dbCursor) = self.executeOneQuery(
+            "SELECT username,email,profile_picture FROM users WHERE username=:username",
+            {"username": username},
         )
-        rawResult = self.getResults()
+        rawResult = self.getResults(dbCursor)[0]
+        dbConn.close()
 
         if rawResult == []:
             return None
 
+        print(rawResult)
         formattedResults = {
             "username": rawResult[0],
             "email": rawResult[1],
@@ -166,17 +205,48 @@ class Db:
         }
         return formattedResults
 
+    def isUsernameTaken(self, username: str) -> bool:
+        (dbConn, dbCursor) = self.executeOneQuery(
+            "SELECT 1 FROM users WHERE username=:name",
+            {
+                "name": username,
+            },
+        )
+        res = self.getResults(dbCursor)
+        dbConn.close()
+        if len(res) > 0:
+            return True
+        else:
+            return False
+
+    def isEmailTaken(self, email: str) -> bool:
+        (dbConn, dbCursor) = self.executeOneQuery(
+            "SELECT 1 FROM users WHERE email=:email",
+            {
+                "email": email,
+            },
+        )
+        res = self.getResults(dbCursor)
+        dbConn.close()
+        if len(res) > 0:
+            return True
+        else:
+            return False
+
     def validateUser(self, userName: str, password: str) -> dict[str, str] | None:
         """Returns details if combination is valid, `None` otherwise."""
-        self.executeOneQuery(
-            "SELECT (username,email,profile_picture) FROM users WHERE username=:name AND password=:password",
-            {"username": userName, "password": password},
+        (dbConn, dbCursor) = self.executeOneQuery(
+            "SELECT username,email,profile_picture FROM users WHERE username=:name AND password=:password",
+            {"name": userName, "password": password},
         )
         # Get atmost one result
-        raw_result = self.getResults(count=1)
+        raw_result = self.getResults(dbCursor, count=1)
+        dbConn.close()
         if raw_result == []:
             return None
         else:
+            print(raw_result)
+            raw_result = raw_result[0]
             return {
                 "username": raw_result[0],
                 "email": raw_result[1],
@@ -185,17 +255,48 @@ class Db:
 
     # The file object here is typed in a flask-specific manner. This will not work for other web frameworks.
     def createUser(
-        self, username: str, password: str, email: str, profile_picture: FileStorage
+        self,
+        username: str,
+        password: str,
+        email: str,
+        profile_picture: FileStorage | None,
     ) -> None:
         """The main function to generate entries for a new user.
 
         This function expects the parameters to be dumped in directly from the `request` object returned by a flask server.
         """
-        if profile_picture_path is None:
-            profile_picture_path = ERROR_ICON_PATH
+        profile_picture_path: str | None
+        # profile_picture_path_tmp: str | None = self.upload_image(profile_picture)
+        # if profile_picture_path_tmp is None:
+        #     profile_picture_path = ERROR_ICON_PATH
+        # else:
+        #     profile_picture_path = profile_picture_path_tmp
+        # profile_picture_path = ERROR_ICON_PATH
+        if (profile_picture is None) or (profile_picture.filename is None):
+            profile_picture_path = None
         else:
-            profile_picture_path = self.upload_image(profile_picture.stream)
-        self.executeOneQuery(
+            profile_picture_path = self.upload_image(profile_picture)
+            # imgExt = profile_picture.filename.rsplit(".", 1)[1].lower()
+            # md5 = hashlib.md5()
+            # # Break out of loop manually once the entire file has been read
+            # imgStream = profile_picture.stream
+            # while True:
+            #     # Read data in chunks to avoid running out of memory.
+            #     data = imgStream.read(BUF_SIZE)
+            #     if not data:
+            #         break
+            #     md5.update(data)
+            # # The final file name
+            # fileName = md5.hexdigest()
+            # profile_picture_path = "/".join(
+            #     ["static", IMAGE_FOLDER, f"{fileName}.{imgExt}"]
+            # )
+            # print(profile_picture_path)
+            # imgStream.seek(0)
+            # with open(pathlib.Path(profile_picture_path), "wb") as f:
+            #     f.write(imgStream.read())
+            # # profile_picture.save(profile_picture_path)
+        (dbConn, dbCursor) = self.executeOneQuery(
             "INSERT INTO users (username, email, password, profile_picture) VALUES (:username, :email, :password, :pic_path)",
             {
                 "username": username,
@@ -204,59 +305,91 @@ class Db:
                 "pic_path": profile_picture_path,
             },
         )
+        dbConn.close()
+
+    def deleteUser(self, username: str) -> bool:
+        (dbConn, dbCursor) = self.executeOneQuery(
+            "DELETE FROM users WHERE username = :name",
+            {
+                "name": username,
+            },
+        )
+        dbConn.close()
+        return True
+
+    def updatePassword(self, username: str, oldPassword: str, newPassword: str) -> bool:
+        usr = self.validateUser(userName=username, password=oldPassword)
+        if usr is None:
+            return False
+        else:
+            (dbConn, dbCursor) = self.executeOneQuery(
+                "UPDATE users SET password = :password WHERE username = :username",
+                {
+                    "username": username,
+                    "password": newPassword,
+                },
+            )
+            dbConn.close()
+            return True
+
+    def updatePicture(self, username, new_profile_picture) -> bool:
+        new_pic_path = self.upload_image(new_profile_picture)
+        if new_pic_path is None:
+            return False
+        (dbConn, dbCursor) = self.executeOneQuery(
+            "UPDATE users SET profile_picture = :pic WHERE username = :name",
+            {
+                "name": username,
+                "pic": new_pic_path,
+            },
+        )
+        dbConn.close()
+        return True
+
+    def dumpUsers(self) -> list[typing.Any]:
+        (dbConn, dbCursor) = self.executeOneQuery("SELECT * FROM users")
+        res = self.getResults(dbCursor=dbCursor)
+        dbConn.close()
+        return res
 
     def upload_image(
         self,
-        imageStream: IO[bytes],
-        imageExtension: str = "",
-        uploadedFileName: str | None = None,
+        image: FileStorage,
     ) -> str | None:
         """Uploads an image and returns the ref to the image in the database. Returns `None` if the file is invalid or was not stored.
 
         It is highly reccomended that the `iamegExtension` or `uploadedFileName` is set, ideally using .
 
-        On the disk, the file is stored as `static/images/<file-md5-hash>.<file-extension>`.
+        On the disk, the file is stored as `static/IMAGE_FOLDER/<file-md5-hash>.<file-extension>`.
 
         `md5` is used as it is fast and we don't care too much about hash collisions.
 
         Note that this removes the original file from disk.
         """
-        # File hashing implementation taken from https://stackoverflow.com/a/22058673
+        profile_picture = image
+        if profile_picture.filename is None:
+            return None
+        imgExt = profile_picture.filename.rsplit(".", 1)[1].lower()
         md5 = hashlib.md5()
         # Break out of loop manually once the entire file has been read
+        imgStream = profile_picture.stream
         while True:
             # Read data in chunks to avoid running out of memory.
-            data = imageStream.read(BUF_SIZE)
+            data = imgStream.read(BUF_SIZE)
             if not data:
                 break
             md5.update(data)
         # The final file name
         fileName = md5.hexdigest()
-        if (imageExtension == "") and (not (uploadedFileName is None)):
-            imageExtension = getImageExtensionFromFilename(uploadedFileName)
-
-        fileName = f"{fileName}.{imageExtension}"
-
-        imagePath = pathlib.Path(
-            os.path.join("static", "images", f"{fileName}.{imageExtension}")
+        profile_picture_path = "/".join(
+            ["static", IMAGE_FOLDER, f"{fileName}.{imgExt}"]
         )
-
-        if not (imagePath.exists()):
-            # Skip writing file to disk if it already exists.
-
-            # Open file for writing binary data
-            imageHandle = imagePath.open("+wb")
-
-            while True:
-                # Read data in chunks to avoid running out of memory.
-                data = imageStream.read(BUF_SIZE)
-                if not data:
-                    break
-                imageHandle.write(data)
-
-            imageHandle.close()
-
-        return imagePath
+        print(profile_picture_path)
+        imgStream.seek(0)
+        with open(pathlib.Path(profile_picture_path), "wb") as f:
+            f.write(imgStream.read())
+        return profile_picture_path
+        # profile_picture.save(profile_picture_path)
 
 
 def getImageExtensionFromFilename(filename: str) -> str:
